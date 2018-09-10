cdb74545c6d2189e1264d325210aaa8051dcba67
nisheejaiswal <jaiswalnishee2@gmail.com>
2018-09-10 12:54:55 +0530

    Add files via upload

---
+++ ComparisonOperator.py
@@ -0,0 +1,24 @@
+
+# Python Comparision Operator
+
+
+x = 5
+y = 6
+
+#Equal
+print(x == y)
+
+#Not equal
+print(x != y)
+
+#Greater than
+print(x > y)
+
+#Less than
+print(x < y)
+
+#Greater than or equal to
+print(x >= y)
+
+#Less than or equal to
+print(x <= y)
