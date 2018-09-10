1b1a22c5dd44b6d5a9aba7a7825f00836fa6951c
nisheejaiswal <jaiswalnishee2@gmail.com>
2018-09-10 12:54:41 +0530

    Add files via upload

---
+++ BitwiseOperators.py
@@ -0,0 +1,27 @@
+
+# Python Bitwise Operators
+
+x = 5
+y = 6
+
+# & Operator (AND)
+print(x & y)
+
+# | Operator (OR)
+print(x | y)
+
+# ^ Operator (XOR)
+print(x ^ y)
+
+# ~ Operator (NOT)
+print(~x)
+
+# << Operator (Zero fill left shift)
+a = 6
+b = 2
+print(a << b)
+
+# >> Operator (Signed right shift)
+a = 6
+b = 2
+print(a >> b)
