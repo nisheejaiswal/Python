1af25cc359850227c69222c0618dca5bc168877d
nisheejaiswal <jaiswalnishee2@gmail.com>
2018-09-10 12:54:11 +0530

    Add files via upload

---
+++ AssignmentOperators.py
@@ -0,0 +1,53 @@
+
+# Python Assignment Operators
+
+x = 5
+print(x)
+
+x = 5
+x = x + 3
+print(x)
+
+x = 5
+x = x - 3
+print(x)
+
+x = 5
+x = x * 3
+print(x)
+
+x = 5
+x = x / 3
+print(x)
+
+x = 5
+x = x % 3
+print(x)
+
+x = 5
+x = x // 3
+print(x)
+
+x = 5
+x = x ** 3
+print(x)
+
+x = 5
+x = x & 3
+print(x)
+
+x = 5
+x = x | 3
+print(x)
+
+x = 5
+x = x ^ 3
+print(x)
+
+x = 5
+x = x >> 3
+print(x)
+
+x = 5
+x = x << 3
+print(x)
