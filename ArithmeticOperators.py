d2d250a6fbde96f89454905a32392ffc8ad99ece
nisheejaiswal <jaiswalnishee2@gmail.com>
2018-09-10 12:53:58 +0530

    Add files via upload

---
+++ ArithmeticOperators.py
@@ -0,0 +1,24 @@
+
+# Python Arithmetic Operators
+
+x = 9
+y = 7
+
+#Addition
+print(x+y)
+
+#Subtraction
+print(x-y)
+
+#Multiplication
+print(x*y)
+
+#Division
+print(x/y)
+
+#Exponentiation
+print(x**y)
+
+#Floor Division
+print(x//y)
+
