a815423845b5953929417b9fc5fbca45d06fffd2
nisheejaiswal <jaiswalnishee2@gmail.com>
2018-09-10 11:56:52 +0530

    Add files via upload

---
+++ CollectionsTuple.py
@@ -0,0 +1,33 @@
+
+# Tuple - ordered and unchangeable
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# Access tuple Items
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+# Change Tuple Values - Once a tuple is creates, we cannot change its values.
+
+# Loop Through a Tuple
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+    print(x)
+
+# Tuple Length
+print(len(thistuple))
+
+# Add Items - We cannot add items
+
+# Remove Items
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+
+# The tuple() Constructor
+thistuple = tuple(("apple", "banana", "cherry"))
+print(thistuple)
+
+
+
+
+
