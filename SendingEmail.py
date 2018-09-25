b8f29390c637ed5a40bcf20ea6ddd7d12d29cc57
nisheejaiswal <jaiswalnishee2@gmail.com>
2018-09-25 20:39:31 +0530

    Update SendingEmail.py

--- SendingEmail.py
+++ SendingEmail.py
@@ -5,7 +5,7 @@ content = 'example email stuff here'
 Server = smtplib.SMTP('smtp.gmail.com', 587)
 Server.starttls()
 
-Server.login("jaiswalnishee2@gmail.com","Papa11.09.1996")
+Server.login("jaiswalnishee2@gmail.com","xxxxxxxx")
 
 Server.sendmail('jaiswalnishee2@gmail.com','nishee.jaiswal@gslab.com', content)
 print("Email send Successfully")
